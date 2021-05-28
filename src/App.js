dd311ef72de816244fcb186fbc4395e992610954
alelorduy <alejandra_lorduy@hotmail.com>
2021-05-27 21:13:19 -0400

    Create checked task

--- src/App.js
+++ src/App.js
@@ -1,34 +1,61 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
 
-/// Modifica el componente para que se puedan agregar tareas, tachar y destacharlas y error de validacion en el input
+/// Modifica el componente para que se puedan agregar tareas
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      tasks: [
-        { id: 1, name: "Sacar la ropa", done: false },
-        { id: 2, name: "Hacer la cama", done: true },
-        { id: 3, name: "Leer un rato", done: false }
-      ],
-      newTask: ''
+function Tasks() {
+  const [taskList, setTaskList] = useState([
+    { id: 1, name: "Sacar la ropa", done: false },
+    { id: 2, name: "Hacer la cama", done: false },
+    { id: 3, name: "Leer un rato", done: false },
+  ]);
+  const [newTask, setNewTask] = useState("");
+
+  const sendNewTask = (event) => {
+    event.preventDefault();
+    if (newTask) {
+      let obj = { id: taskList.length + 1, name: newTask, done: false };
+      setTaskList([...taskList, obj]);
+      setNewTask("");
     }
-  }
-  render() {
-    return (
-      <div className="wrapper">
-        <div className="list">
-          <h3>Por hacer:</h3>
-          <ul className="todo">
-            {this.state.tasks.map((task, index) => <li key={task.id}>{task.name}</li>)}
-          </ul>
-          <form>
-            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} />
-          </form>
-        </div>
-      </div>
-    )
-  }
-}
+  };
 
-export default App;
+  const checkedTask = (id) => {
+    const newArray = taskList.map((task) => {
+      if (task.id === id) {
+        task.done = !task.done;
+      }
+      return task;
+    });
+    setTaskList(newArray);
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="list">
+        <h3>Por hacer:</h3>
+        <ul className="todo">
+          {taskList.map((task) => (
+            <li
+              className={task.done ? "done" : ""}
+              onClick={() => checkedTask(task.id)}
+            >
+              {task.name}
+            </li>
+          ))}
+        </ul>
+        <form onSubmit={(event) => sendNewTask(event)}>
+          <input
+            className={!newTask ? "error" : ""}
+            value={newTask}
+            type="text"
+            id="new-task"
+            placeholder="Ingresa una tarea y oprime Enter"
+            //onfocus="if(this.value == 'value') { this.value = ''; }"
+            onChange={(event) => setNewTask(event.target.value)}
+          />
+        </form>
+      </div>
+    </div>
+  );
+}
+export default Tasks;
